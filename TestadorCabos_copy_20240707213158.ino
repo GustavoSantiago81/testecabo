cf1825bf3b05efc9c1b28be0eeeb31c93f28f378
Gustavo Santiago Pereira <123910445+GustavoSantiago81@users.noreply.github.com>
2024-07-26 20:55:04 -0300

    tedtge q git config

--- TestadorCabos_copy_20240707213158.ino
+++ TestadorCabos_copy_20240707213158.ino
@@ -1,81 +1,275 @@
-/* Testador de Cabos */
+/*------- TESTADOR DE CABOS ------- */
+//----- Definições dos pinos das Vias --------
+#define canalBranco 38
+#define canalPreto 39
+#define canalVerde 40
+#define canalVermelho 41
+#define buzzer 42
+#define but_cl 2
+#define but_cm 1
 
-#define buzzer 10
-char um ='u';
-char cadeia[]="cadeia";
-char teste[]={2,1};
-char db9[] = {20, 21, 47, 48, 45, 0, 35, 36, 37};   // 9 pinos, quais poderia usar um mux
-char binario[9];
-char ports[4] = {11, 12, 13, 14};               // 4 vias, nesse caso
-bool branco = false, preto = false, verde = false, vermelho = false;
+char db9[] = {47, 48, 45, 0, 35, 21, 20, 37, 36};                                   // 9 pinos, quais estão na ordem do Pino 1 ao Pino 9
+//char ports[4] = {38, 39, 40, 41};
+char ports[4] = {canalBranco, canalPreto, canalVerde, canalVermelho};               // 4 vias, nesse caso
+bool brancoIsOkay = false, pretoIsOkay = false, verdeIsOkay = false, vermelhoIsOkay = false;
+int countBranco = 0, countPreto = 0, countVerde = 0, countVermelho = 0;
 
 void setup() {
   Serial.begin(115200);
+  pinMode(but_cl, INPUT_PULLUP);
+  pinMode(but_cm, INPUT_PULLUP);
   // Configurando os pinos de entrada
-  for (int i = 0; i < 9; i++) {
+  for (int i = 0; i < 9; i++) 
+  {
     pinMode(db9[i], INPUT);
   }
 
   // Configurando os pinos de saída
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 4; i++) 
+  {
     pinMode(ports[i], OUTPUT);
   }  
+
+  pinMode(buzzer, OUTPUT);
+  digitalWrite(buzzer, LOW);
 }
 
-void loop() {
-  
-  for(int i = 0; i < 4; i++) 
+void loop() 
+{
+  if(digitalRead(but_cl) == LOW){
+    checkCL();
+  }
+  if(digitalRead(but_cm) == LOW){
+    checkCM();
+  }
+}
+
+void checkCL()
+{
+  for(int i = 0; i < 4; i++)
   {
     digitalWrite(ports[i], HIGH);
+    delay(100);
+    if(ports[i] == canalBranco)
+    {
+      for(int i = 0; i < 9; i++)
+      {
+        delay(100);
+        if(digitalRead(db9[i] == HIGH))
+        {
+          brancoIsOkay = false;
 
-    if((digitalRead(37) == HIGH)&&ports[i]==0)
-    {
-      if((digitalRead(20)||digitalRead(48)||digitalRead(45)||digitalRead(0)||digitalRead(35)||digitalRead(36)||digitalRead(21)||digitalRead(47))== LOW)
+          if(digitalRead(db9[6 - 1]) == HIGH || digitalRead(db9[9 - 1]) == HIGH)
+            countBranco++;
+        }
+      }
+
+      if(countBranco == 2)
       {
-        branco = true;
+        brancoIsOkay = true;
       }
     }
-    if((digitalRead(45)&&digitalRead(47) == HIGH)&&ports[i]==1)
+
+    if(ports[i] == canalPreto)
     {
-      if((digitalRead(20)||digitalRead(48)||digitalRead(45)||digitalRead(0)||digitalRead(35)||digitalRead(36)||digitalRead(21))== LOW)
+      for(int i = 0; i < 9; i++)
       {
-        preto = true;
+        delay(100);
+        if(digitalRead(db9[i] == HIGH))
+        {
+          pretoIsOkay = false;
+
+          if(digitalRead(db9[5 - 1]) == HIGH || digitalRead(db9[3 - 1]) == HIGH)
+            countPreto++;
+        }
+      }
+
+      if(countPreto == 2)
+      {
+        pretoIsOkay = true;
       }
     }
-    if((digitalRead(21)&&digitalRead(48)&&digitalRead(46) == HIGH)&&ports[i]==2)
+
+    if(ports[i] == canalVerde)
     {
-      if((digitalRead(20)||digitalRead(47)||digitalRead(45)||digitalRead(0)||digitalRead(35)||digitalRead(36)||digitalRead(37))==LOW)
+      for(int i = 0; i < 9; i++)
       {
-        verde = true;
+        delay(100);
+        if(digitalRead(db9[i] == HIGH))
+        {
+          verdeIsOkay = false;
+          delay(100);
+          if(digitalRead(db9[2 - 1]) == HIGH || digitalRead(db9[4 - 1]) == HIGH || digitalRead(db9[8 - 1]) == HIGH)
+            countVerde++;
+        }
+      }
+
+      if(countVerde == 3)
+      {
+        verdeIsOkay = true;
       }
     }
-    if((digitalRead(20)&&digitalRead(35) == HIGH)&&ports[i]==3)
+
+    if(ports[i] == canalVermelho)
     {
-      if((digitalRead(21)||digitalRead(48)||digitalRead(45)||digitalRead(0)||digitalRead(47)||digitalRead(36)||digitalRead(37))== LOW)
+      for(int i = 0; i < 9; i++)
       {
-        vermelho = true;
+        delay(100);
+        if(digitalRead(db9[i] == HIGH))
+        {
+          vermelhoIsOkay = false;
+
+          if(digitalRead(db9[1 - 1]) == HIGH || digitalRead(db9[7 - 1]) == HIGH)
+            countVermelho++;
+        }
+      }
+
+      if(countVermelho == 2)
+      {
+        vermelhoIsOkay = true;
       }
     }
-  
     digitalWrite(ports[i], LOW);
   }
-
-  if(branco, preto, verde, vermelho)
+  
+  if(brancoIsOkay&&pretoIsOkay&&verdeIsOkay&&vermelhoIsOkay)
   {
     digitalWrite(buzzer, HIGH);
-    delay(100);
+    delay(200);
     digitalWrite(buzzer, LOW);
-    delay(100);
+    delay(200);
+    digitalWrite(buzzer, HIGH);
+    delay(200);
+    digitalWrite(buzzer, LOW);
+    delay(200);
   }
   else
   {
     digitalWrite(buzzer, HIGH);
     delay(1000);
     digitalWrite(buzzer, LOW);
-    delay(100);
+    delay(400);
+    digitalWrite(buzzer, HIGH);
+    delay(1000);
+    digitalWrite(buzzer, LOW);
+    delay(200);
   }
-  branco =false;
-  preto=false;
-  verde=false;
-  vermelho=false;
+  brancoIsOkay=false;pretoIsOkay=false;verdeIsOkay=false;vermelhoIsOkay=false;
+  countBranco=0;countPreto=0;countVerde=0;countVermelho=0;
+}
+
+
+/* CARDIOMAPA */
+void checkCM()
+{
+  for(int i = 0; i < 4; i++)
+  {
+    digitalWrite(ports[i], HIGH);
+    delay(100);
+    if(ports[i] == canalBranco)
+    {
+      for(int i = 0; i < 7; i++)
+      {
+        delay(100);
+        if(digitalRead(db9[i] == HIGH))
+        {
+          brancoIsOkay = false;
+
+          if(digitalRead(db9[3 - 1]) == HIGH || digitalRead(db9[5 - 1]) == HIGH)
+            countBranco++;
+        }
+      }
+
+      if(countBranco == 2)
+      {
+        brancoIsOkay = true;
+      }
+    }
+
+    if(ports[i] == canalPreto)
+    {
+      for(int i = 0; i < 7; i++)
+      {
+        delay(100);
+        if(digitalRead(db9[i] == HIGH))
+        {
+          pretoIsOkay = false;
+
+          if(digitalRead(db9[2 - 1]) == HIGH || digitalRead(db9[4 - 1]) == HIGH)
+            countPreto++;
+        }
+      }
+
+      if(countPreto == 2)
+      {
+        pretoIsOkay = true;
+      }
+    }
+
+    if(ports[i] == canalVerde)
+    {
+      for(int i = 0; i < 7; i++)
+      {
+        delay(100);
+        if(digitalRead(db9[i] == HIGH))
+        {
+          verdeIsOkay = false;
+          delay(100);
+          if(digitalRead(db9[7 - 1]) == HIGH)
+            countVerde++;
+        }
+      }
+
+      if(countVerde == 1)
+      {
+        verdeIsOkay = true;
+      }
+    }
+
+    if(ports[i] == canalVermelho)
+    {
+      for(int i = 0; i < 7; i++)
+      {
+        delay(100);
+        if(digitalRead(db9[i] == HIGH))
+        {
+          vermelhoIsOkay = false;
+
+          if(digitalRead(db9[1 - 1]) == HIGH || digitalRead(db9[6 - 1]) == HIGH)
+            countVermelho++;
+        }
+      }
+
+      if(countVermelho == 2)
+      {
+        vermelhoIsOkay = true;
+      }
+    }
+    digitalWrite(ports[i], LOW);
+  }
+  
+  if(brancoIsOkay&&pretoIsOkay&&verdeIsOkay&&vermelhoIsOkay)
+  {
+    digitalWrite(buzzer, HIGH);
+    delay(200);
+    digitalWrite(buzzer, LOW);
+    delay(200);
+    digitalWrite(buzzer, HIGH);
+    delay(200);
+    digitalWrite(buzzer, LOW);
+    delay(200);
+  }
+  else
+  {
+    digitalWrite(buzzer, HIGH);
+    delay(1000);
+    digitalWrite(buzzer, LOW);
+    delay(400);
+    digitalWrite(buzzer, HIGH);
+    delay(1000);
+    digitalWrite(buzzer, LOW);
+    delay(200);
+  }
+  brancoIsOkay=false;pretoIsOkay=false;verdeIsOkay=false;vermelhoIsOkay=false;
+  countBranco=0;countPreto=0;countVerde=0;countVermelho=0;
 }
